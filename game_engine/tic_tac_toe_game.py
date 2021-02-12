20fa9bb044f7b41ae0fbe969a08466b4a2b0713a
Vadim Rusakov <darksteellord@gmail.com>
2021-02-12 19:42:21 +0300

    protected properties fixed

--- game_engine/tic_tac_toe_game.py
+++ game_engine/tic_tac_toe_game.py
@@ -8,15 +8,15 @@ class TicTacToeGame(AbstractTicTacToeGame):
 
     def __init__(self, game_id: str, first_player_id: str, second_player_id: str,
                  strategy: Optional[Callable[[TicTacToeGameInfo], TicTacToeTurn]] = None) -> None:
-        self.__game_id = game_id
-        self.__first_player_id = first_player_id
-        self.__second_player_id = second_player_id
-        self.__winner_id = ""
-        self.__strategy = strategy
-        self.__turns: List[TicTacToeTurn] = []
+        self._game_id = game_id
+        self._first_player_id = first_player_id
+        self._second_player_id = second_player_id
+        self._winner_id = ""
+        self._strategy = strategy
+        self._turns: List[TicTacToeTurn] = []
 
     def is_turn_correct(self, turn: TicTacToeTurn) -> bool:
-        if self.__winner_id != "":
+        if self._winner_id != "":
             return False
 
         if self.current_player_id() != turn.player_id:
@@ -28,29 +28,29 @@ class TicTacToeGame(AbstractTicTacToeGame):
         return self.get_game_info().field[turn.x_coordinate][turn.y_coordinate] == " "
 
     def current_player_id(self) -> str:
-        if not self.__turns:
-            return self.__first_player_id
+        if not self._turns:
+            return self._first_player_id
 
-        if self.__turns[-1].player_id == self.__first_player_id:
-            return self.__second_player_id
+        if self._turns[-1].player_id == self._first_player_id:
+            return self._second_player_id
 
-        return self.__first_player_id
+        return self._first_player_id
 
     def do_turn(self, turn: TicTacToeTurn) -> TicTacToeGameInfo:
         if not self.is_turn_correct(turn):
             return self.get_game_info()
 
-        self.__turns.append(deepcopy(turn))
+        self._turns.append(deepcopy(turn))
         self._set_winner()
 
-        if self.__winner_id == "" and self.__strategy is not None:
-            self.__turns.append(self.__strategy(self.get_game_info()))
+        if self._winner_id == "" and self._strategy is not None:
+            self._turns.append(self._strategy(self.get_game_info()))
             self._set_winner()
 
         return self.get_game_info()
 
     def _set_winner(self) -> None:
-        if self.__winner_id != "":
+        if self._winner_id != "":
             return
 
         field = self.get_game_info().field
@@ -64,11 +64,11 @@ class TicTacToeGame(AbstractTicTacToeGame):
                 row2.append(field[j][i])
 
             if row1 == ["X"] * 3 or row2 == ["X"] * 3:
-                self.__winner_id = self.__first_player_id
+                self._winner_id = self._first_player_id
                 return
 
             if row1 == ["O"] * 3 or row2 == ["O"] * 3:
-                self.__winner_id = self.__second_player_id
+                self._winner_id = self._second_player_id
                 return
 
             if ("X" not in row1) or ("X" not in row2) or ("O" not in row1) or ("O" not in row2):
@@ -83,34 +83,34 @@ class TicTacToeGame(AbstractTicTacToeGame):
             row2.append(field[i][2 - i])
 
         if row1 == ["X"] * 3 or row2 == ["X"] * 3:
-            self.__winner_id = self.__first_player_id
+            self._winner_id = self._first_player_id
             return
 
         if row1 == ["O"] * 3 or row2 == ["O"] * 3:
-            self.__winner_id = self.__second_player_id
+            self._winner_id = self._second_player_id
             return
 
         if ("X" not in row1) or ("X" not in row2) or ("O" not in row1) or ("O" not in row2):
             draw = False
 
         if draw:
-            self.__winner_id = "draw"
+            self._winner_id = "draw"
 
     def get_game_info(self) -> TicTacToeGameInfo:
         result = TicTacToeGameInfo(
-            game_id=self.__game_id,
+            game_id=self._game_id,
             field=[
                 [" ", " ", " "],
                 [" ", " ", " "],
                 [" ", " ", " "]
             ],
-            sequence_of_turns=deepcopy(self.__turns),
-            first_player_id=self.__first_player_id,
-            second_player_id=self.__second_player_id,
-            winner_id=self.__winner_id
+            sequence_of_turns=deepcopy(self._turns),
+            first_player_id=self._first_player_id,
+            second_player_id=self._second_player_id,
+            winner_id=self._winner_id
         )
-        for turn in self.__turns:
-            if turn.player_id == self.__first_player_id:
+        for turn in self._turns:
+            if turn.player_id == self._first_player_id:
                 symb = "X"
             else:
                 symb = "O"
